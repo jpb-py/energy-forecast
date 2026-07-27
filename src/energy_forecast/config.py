a2da9cbebd065f14ce74190236c0656e5ab44d38
Jonny Black <black_jon@hotmail.com>
2026-07-27 11:09:37 +0100

    Update config file to default parameters

--- src/energy_forecast/config.py
+++ src/energy_forecast/config.py
@@ -2,11 +2,11 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class BatteryParams:
-    c_max: 0.2
-    d_max: 0.2
-    s_max: 1
-    eta: 0.9
-    t: 0.5
-    k: 0.003
-    s_start: 0.5
-    s_end: 0.5
+    c_max: float = 0.5
+    d_max: float = 0.5
+    s_max: float = 1.0
+    eta: float = 0.9
+    t: float = 0.5
+    k: float = 0.003
+    s_start: float = 0.5
+    s_end: float = 0.5
